302647e7fbfeb3f9af38df8635bf986fe6619a6b
mteusbarbosa <mateus.bm13@gmail.com>
2023-02-10 14:35:50 -0300

    criação do componente Banner

---
+++ organo/src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css"
+
+export default function Banner() {
+    return (
+        <>
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="O banner principal da página do organo" className="banner" />
+            </header>
+        </>
+    )
+}
