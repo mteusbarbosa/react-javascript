52c7e0d6ef0d6a11c093a60b9a5fcd55ba686ab2
mteusbarbosa <mateus.bm13@gmail.com>
2023-03-31 11:01:40 -0300

    Colaborador component

---
+++ organo/src/components/Colaborador/Colaborador.jsx
@@ -0,0 +1,13 @@
+import './Colaborador.css'
+
+export default function Colaborador(props){
+    return <div className='colaborador'>
+        <div className='cabecalho'>
+            <img src={props.imagem} alt={props.nome} />
+        </div>
+        <div className='rodape'>
+            <h4>{props.nome}</h4>
+            <h5>{props.cargo}</h5>
+        </div>
+    </div>
+}
