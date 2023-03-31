48426f437539ce94c83c5b4b7791f054be216141
mteusbarbosa <mateus.bm13@gmail.com>
2023-03-31 11:01:49 -0300

    Time component

--- organo/src/components/Time/Time.jsx
+++ organo/src/components/Time/Time.jsx
@@ -1,12 +1,25 @@
+import Colaborador from "../Colaborador/Colaborador";
 import "./Time.css";
 
 export default function Time(props) {
   const { nome, corPrimaria, corSecundaria } = props;
   return (
     <>
-      <section className="time" style={{ backgroundColor: corSecundaria }}>
-        <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
-      </section>
+      {props.colaboradores.length > 0 && (
+        <section className="time" style={{ backgroundColor: corSecundaria }}>
+          <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
+          <div className="colaboradores">
+            {props.colaboradores.map((item) => (
+              <Colaborador
+                key={item}
+                nome={item.nome}
+                cargo={item.cargo}
+                imagem={item.imagem}
+              />
+            ))}
+          </div>
+        </section>
+      )}
     </>
   );
 }
