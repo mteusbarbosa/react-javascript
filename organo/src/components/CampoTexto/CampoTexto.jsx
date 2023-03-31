b39ba16a7a880fd8870a71fa91700e1fe49401df
mteusbarbosa <mateus.bm13@gmail.com>
2023-03-30 21:04:16 -0300

    Refatorando CampoTexto

--- organo/src/components/CampoTexto/CampoTexto.jsx
+++ organo/src/components/CampoTexto/CampoTexto.jsx
@@ -1,10 +1,11 @@
-import "./CampoTexto.css"
+import "./CampoTexto.css";
 
 export default function CampoTexto(props) {
+  
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input placeholder={props.placeholder} />
+      <input placeholder={props.placeholder} value={props.valor} onChange={event => props.aoAlterado(event.target.value)} required />
     </div>
   );
 }
