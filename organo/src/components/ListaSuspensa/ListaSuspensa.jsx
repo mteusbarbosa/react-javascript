a9431418bcf171ed0cbf91e3ed8ef094345b68e3
mteusbarbosa <mateus.bm13@gmail.com>
2023-03-30 21:05:22 -0300

    ListaSuspensa component

--- organo/src/components/ListaSuspensa/ListaSuspensa.jsx
+++ organo/src/components/ListaSuspensa/ListaSuspensa.jsx
@@ -3,9 +3,9 @@ import "./ListaSuspensa.css"
 export default function ListaSuspensa(props){
     return <div className="lista-suspensa">
         <label htmlFor="">{props.label}</label>
-        <select name="" id="">
+        <select onChange={event => props.aoAlterado(event.target.value)} >
             {props.itens.map(item => {
-                return <option key={item}>{item}</option>
+                return <option key={item} value={item} >{item}</option>
             })}
         </select>
     </div>
