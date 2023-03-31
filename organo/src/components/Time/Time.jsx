3ac740bc0c2819152baa4c6de16365aad90f07ab
mteusbarbosa <mateus.bm13@gmail.com>
2023-03-30 21:29:29 -0300

    Time component

--- organo/src/components/Time/Time.jsx
+++ organo/src/components/Time/Time.jsx
@@ -1,9 +1,12 @@
 import "./Time.css";
 
 export default function Time(props) {
-  return <>
-    <section className="time">
-        <h3>{props.nome}</h3>
-    </section>
-  </>;
+  const { nome, corPrimaria, corSecundaria } = props;
+  return (
+    <>
+      <section className="time" style={{ backgroundColor: corSecundaria }}>
+        <h3 style={{ borderColor: corPrimaria }}>{nome}</h3>
+      </section>
+    </>
+  );
 }
